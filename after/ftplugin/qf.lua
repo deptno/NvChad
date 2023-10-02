0ddf03091c117b25337a3879c980f09c5caf1375
deptno <deptno@gmail.com>
2023-10-02 11:09:38 +0900

    fix: qf 가 열릴때는 BufWinEnter 를 사용해야한다 + https://vi.stackexchange.com/questions/14065/autocmd-with-quickfix-window/14068#14068

---
+++ after/ftplugin/qf.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = vim.api.nvim_create_augroup("QfFiletype", { clear = true }),
+  callback = function(ev)
+    local opts = {}
+
+    vim.api.nvim_buf_set_keymap(ev.buf, "n", "q", ":lclose<CR>", opts)
+  end
+})
