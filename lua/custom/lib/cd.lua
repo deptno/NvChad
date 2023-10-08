f142af89efda55cf3d8deb9f76dad2ed7aae1327
deptno <deptno@gmail.com>
2023-10-08 16:24:06 +0900

    cd 명령어때 print 대신 notify 사용

--- lua/custom/lib/cd.lua
+++ lua/custom/lib/cd.lua
@@ -1,6 +1,8 @@
 local function cd(path)
-  print("cd " .. path)
   vim.api.nvim_set_current_dir(path)
+  vim.notify(string.gsub(path, vim.fn.expand('~'), '~'), vim.log.levels.INFO, {
+    title = "Change working directory"
+  })
 end
 
 return cd
