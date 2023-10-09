baeabccee3dd2042e3e4f1d59a264d818695c8a8
deptno <deptno@gmail.com>
2023-10-09 14:47:18 +0900

    fix: cd 처리시에 `..` 의 경우 notify시 경로를 제대로 표시하지 못하는 문제 수정

--- lua/custom/lib/cd.lua
+++ lua/custom/lib/cd.lua
@@ -1,6 +1,6 @@
 local function cd(path)
   vim.api.nvim_set_current_dir(path)
-  vim.notify(string.gsub(path, vim.fn.expand('~'), '~'), vim.log.levels.INFO, {
+  vim.notify(vim.fn.fnamemodify(vim.fn.getcwd(), ':~'), vim.log.levels.INFO, {
     title = "Change working directory"
   })
 end
