7fbcc6e28c113612d5d29dd7ca7057e87b3caeab
deptno <deptno@gmail.com>
2023-10-14 00:12:36 +0900

    cd? 에서 git 정보 추가 표시

--- lua/custom/lib/cd.lua
+++ lua/custom/lib/cd.lua
@@ -1,20 +1,25 @@
 local constant = require('custom/lib/constant')
 local get_git_root = require('lab/gx/lib/get_git_root')
 local get_git_branch_info = require('lab/gx/lib/get_git_branch_info')
+local get_git_remote_info = require('lab/gx/lib/get_git_remote_info')
 
 local function notify(title)
   local git_root = get_git_root()
   local git_current_branch = get_git_branch_info()
+  local git_remote_info = get_git_remote_info() or { username = '', repository = '' }
 
   vim.notify(
     string.format([[
+repository: %s/%s
+branch: %s
 git: %s
 cwd: %s
-branch: %s
 ]],
+      git_remote_info.username,
+      git_remote_info.repository,
+      git_current_branch,
       vim.fn.fnamemodify(git_root, ':~'),
-      vim.fn.fnamemodify(vim.fn.getcwd(), ':~'),
-      git_current_branch
+      vim.fn.fnamemodify(vim.fn.getcwd(), ':~')
     ),
     vim.log.levels.INFO,
     { title = title }
