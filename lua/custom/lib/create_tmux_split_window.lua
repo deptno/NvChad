9d6eff8537e0bcd3ff1d1ccb3f6ef298cd1e8ce6
deptno <deptno@gmail.com>
2023-10-13 16:31:36 +0900

    luadoc 형식 주석 적용, vim.notify level down

--- lua/custom/lib/create_tmux_split_window.lua
+++ lua/custom/lib/create_tmux_split_window.lua
@@ -9,14 +9,26 @@ local run_tmux = function(args)
   local sh = args.sh and string.format('$(SHELL) "%s"', args.sh) or ''
   local command = string.format('tmux split-window %s %s %s', direction, wd, sh)
 
-  vim.notify(command)
+  vim.notify(command, vim.log.levels.TRACE)
 
   return vim.fn.system(command)
 end
 
+---
+---tmux split-window -v
+---
+---@param args?  table
+---  - `wd` (string = cwd): working directory
+---  - `sh` (string = ""): tmux panel 에서 사용될 shell 명령어
 local create_tmux_split_window_vertical = function(args)
   return run_tmux(vim.tbl_extend('keep', { direction = 'v' }, args or {}))
 end
+---
+---tmux split-window -h
+---
+---@param args?  table
+---  - `wd` (string = cwd): working directory
+---  - `sh` (string = ""): tmux panel 에서 사용될 shell 명령어
 local create_tmux_split_window_horizontal = function(args)
   return run_tmux(vim.tbl_extend('keep', { direction = 'h' }, args or {}))
 end
