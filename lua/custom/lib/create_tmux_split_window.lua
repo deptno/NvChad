7f1ad28a9be2d8f865606c0d52d2af577dc6135c
deptno <deptno@gmail.com>
2023-10-13 11:54:09 +0900

    tmux 에 shell 명령어를 받을 수 있도록 확장

--- lua/custom/lib/create_tmux_split_window.lua
+++ lua/custom/lib/create_tmux_split_window.lua
@@ -1,17 +1,24 @@
 local is_tmux = type(vim.env.TMUX) == 'string'
-local create_tmux_split_window_vertical = function()
+local run_tmux = function(args)
   if not is_tmux then
     return vim.notify('Not in tmux',vim.log.levels.ERROR)
   end
 
-  vim.fn.system('tmux split-window -v -c "' .. vim.fn.getcwd() .. '"')
+  local direction = string.format('-%s', args.direction)
+  local wd = string.format('-c %s', args.wd or vim.fn.getcwd())
+  local sh = args.sh and string.format('$(SHELL) "%s"', args.sh) or ''
+  local command = string.format('tmux split-window %s %s %s', direction, wd, sh)
+
+  vim.notify(command)
+
+  return vim.fn.system(command)
 end
-local create_tmux_split_window_horizontal = function()
-  if not is_tmux then
-    return vim.notify('Not in tmux',vim.log.levels.ERROR)
-  end
 
-  vim.fn.system('tmux split-window -h -c "' .. vim.fn.getcwd() .. '"')
+local create_tmux_split_window_vertical = function(args)
+  return run_tmux(vim.tbl_extend('keep', { direction = 'v' }, args or {}))
+end
+local create_tmux_split_window_horizontal = function(args)
+  return run_tmux(vim.tbl_extend('keep', { direction = 'h' }, args or {}))
 end
 
 return {
