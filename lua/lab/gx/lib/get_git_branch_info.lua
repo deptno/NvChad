10c043d027f667b8160c34bcfd6dbee435025956
deptno <deptno@gmail.com>
2023-10-15 15:12:24 +0900

    [origin/main: ahead 12] 형태를 지원하기 위해 awk 구문 수정

--- lua/lab/gx/lib/get_git_branch_info.lua
+++ lua/lab/gx/lib/get_git_branch_info.lua
@@ -1,12 +1,20 @@
 local get_git_branch_info = function ()
-  local ret = vim.fn.system([[git branch -vv | grep -e '^*' | awk '{ print $2 " -> " $4 }']]):gsub('\n', '')
+  local ret = vim.fn.systemlist([=[
+git branch -vv | grep -e '^*' | awk '{
+  print $2
+  while (match($0, /\[[^]]+\]/)) {
+    print substr($0, RSTART, RLENGTH)
+    gsub(/\[[^]]+\]/, "", $0)
+  }
+}']=])
+
   local fail_prefix = 'fatal:'
 
-  if ret:sub(1, #fail_prefix) == fail_prefix then
+  if ret[1]:sub(1, #fail_prefix) == fail_prefix then
     return nil, ret
   end
 
-  return ret, 'ok'
+  return table.concat(ret, ' -> ')
 end
 
 return get_git_branch_info
