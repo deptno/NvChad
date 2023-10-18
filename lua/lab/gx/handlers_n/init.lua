aeae45cc31b2b54c3d80950910d40e17750a3f47
deptno <deptno@gmail.com>
2023-10-19 01:14:46 +0900

    lab/gx: <cword> find files 지원, 근데 커서가 한칼 오른쪽으로 감

--- lua/lab/gx/handlers_n/init.lua
+++ lua/lab/gx/handlers_n/init.lua
@@ -5,4 +5,5 @@ return {
   require('lab/gx/handlers_n/github_blob_of_current_file'),
   require('lab/gx/handlers_n/github_commit_file'),
   require('lab/gx/handlers_n/grep'),
+  require('lab/gx/handlers_n/files'),
 }
