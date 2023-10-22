e0fdc3bebcc37331db280b92849ddd1e0b84e405
deptno <deptno@gmail.com>
2023-10-22 19:24:32 +0900

    vimwiki 에서 `;w` 제거 gx가 대신

--- after/ftplugin/vimwiki.lua
+++ after/ftplugin/vimwiki.lua
@@ -17,7 +17,8 @@ vim.api.nvim_create_autocmd( "BufEnter", {
     vim.api.nvim_buf_set_keymap(ev.buf, "n", "H", "<Plug>VimwikiDiaryPrevDay", opts)
     vim.api.nvim_buf_set_keymap(ev.buf, "n", "<S-x>", "<Plug>VimwikiToggleListItemj", opts)
     vim.api.nvim_buf_set_keymap(ev.buf, "n", "tt", ":VimwikiTable<CR>", opts)
-    vim.api.nvim_buf_set_keymap(ev.buf, "n", ";w", ":execute 'VWS /' . expand('<cword>') . '/' <Bar> :lopen<CR>", opts)
+    -- use gx grep instead
+    -- vim.api.nvim_buf_set_keymap(ev.buf, "n", ";w", ":execute 'VWS /' . expand('<cword>') . '/' <Bar> :lopen<CR>", opts) 
     vim.api.nvim_buf_set_keymap(ev.buf, "n", ";b", ":execute 'VWB' <Bar> :lopen<CR>", opts)
 
     convert_to_markdown(ev.buf)
