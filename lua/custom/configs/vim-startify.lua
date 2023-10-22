19bc9b783e622df84e731f411b6051eb84ed2542
deptno <deptno@gmail.com>
2023-10-22 23:01:09 +0900

    auto-session 도입 전에 구현했던 수동 세션 작업 제거

--- lua/custom/configs/vim-startify.lua
+++ lua/custom/configs/vim-startify.lua
@@ -1,37 +1,4 @@
-local get_project_branch_name = require('custom/lib/get_project_branch_name')
-local constant = require('custom/lib/constant')
 local map      = require('custom/lib/map')
-
-local get_last_session = function ()
-  local __LAST__ = '__LAST__'
-  local fn = vim.fn
-  local last_session_path = fn.stdpath('data') .. '/session/' .. __LAST__
-  local current_session = fn.resolve(last_session_path)
-
-  if vim.fs.basename(current_session )== last_session_path then
-    return print("There is no last_session_path")
-  end
-
-  return current_session
-end
-local create_previous_session_link = function(previous_session)
-  if previous_session then
-    local source = '../' .. vim.fs.basename(previous_session)
-
-    vim.fn.system('ln -fs ' .. source .. ' ' .. constant.PREVIOUS_SESSION_LINK_PATH)
-
-    return true
-  end
-
-  return false
-end
-local switch_previous_session = function ()
-  local previous_session = vim.fn.resolve(constant.PREVIOUS_SESSION_LINK_PATH)
-
-  if vim.fn.filereadable(previous_session) then
-    vim.cmd('SLoad ' .. vim.fs.basename(previous_session))
-  end
-end
 
 local get_git_files = function ()
   local untracked = function (v) return { line = '❗' .. v, path = v, } end
@@ -47,21 +14,6 @@ local get_git_files = function ()
 
   return ret
 end
-vim.g.startify_session_before_save_handler = function ()
-  local close_symbols_outline = function ()
-    local success, so = pcall(require('symbols-outline'))
-
-    if success then
-      if so.view:is_open() then
-        so.close_outline()
-        print("close outline")
-      end
-    end
-  end
-
-  require("nvim-tree.api").tree.close()
-  close_symbols_outline()
-end
 -- @legacy https://github.com/deptno/.config/blob/8bb421a122c30b172f3cd8ec9ac0f8894fe46bc5/.config/nvim/lua/user/startify.lua
 vim.g.startify_show_help = 1
 vim.g.startify_show_help_delay = 1
@@ -70,14 +22,9 @@ vim.g.startify_change_to_dir = 0
 vim.g.change_to_vcs_root = 1
 vim.g.startify_custom_header = {}
 vim.g.startify_update_oldfiles = 1
-vim.g.startify_session_persistence = 1
 vim.g.startify_session_autoload = 0
 vim.g.startify_skiplist = { '.*OUTLINE$' }
-vim.g.startify_session_delete_buffers = 1
 vim.g.startify_enable_special = 0
-vim.g.startify_session_before_save = {
-  'lua vim.g.startify_session_before_save_handler()',
-}
 vim.g.startify_bookmarks = {
   { bz = '~/.zshrc' },
   { bg = '~/.gitconfig' },
@@ -86,22 +33,8 @@ vim.g.startify_bookmarks = {
   { bt = '~/.tmux.conf' },
   '~/.taskrc',
 }
--- session list wrapper 를 만들기 귀찮아서 처리
-vim.g.startify_custom_indices = {
-  'c.',
-  'cn',
-  'dn',
-  'p0',
-  'pd',
-  'pt',
-  'ph',
-  'ps',
-  'pz',
-  'w',
-}
-vim.g.switch_previous_session = switch_previous_session
 vim.g.startify_commands = {
-  { l = { "previous session", "= vim.g.switch_previous_session()" }, },
+  -- { l = { "previous session", "= vim.g.switch_previous_session()" }, },
 }
 vim.g.startify_lists = {
     { type = 'commands', header = { '   Commands' } },
@@ -111,49 +44,3 @@ vim.g.startify_lists = {
     { type = 'dir', header = { '   📂 Current directory ' .. vim.fn.getcwd() } },
     { type = 'bookmarks', header = { '   Bookmarks' } },
 }
-
-vim.api.nvim_create_autocmd("SessionLoadPost", {
-  group = vim.api.nvim_create_augroup("StartifyAutoSaveSession", { clear = true }),
-  callback = function()
-    local previous_session = get_last_session()
-
-    if previous_session then
-      local is_created = create_previous_session_link(previous_session)
-
-      if is_created then
-        vim.schedule(function ()
-          local current_session = get_last_session()
-
-          if previous_session ~= current_session then
-            vim.notify(
-              vim.fs.basename(current_session),
-              vim.log.levels.INFO,
-              { title = 'Session changed' }
-            )
-          end
-        end)
-      end
-    end
-  end,
-})
-vim.api.nvim_create_autocmd("VimLeavePre", {
-  group = vim.api.nvim_create_augroup("StartifyVimLeavePreAutoSaveSession", { clear = true }),
-  callback = create_previous_session_link,
-})
-vim.api.nvim_create_user_command(
-  'GetProjectBranchName',
-  get_project_branch_name,
-  {}
-)
-vim.cmd [[
-function! GetUniqueSessionName()
-  let path = fnamemodify(getcwd(), ':~:t')
-  let path = empty(path) ? 'no-project' : path
-  let branch = gitbranch#name()
-  let branch = empty(branch) ? '' : '-' . branch
-  return substitute(path . branch, '/', '-', 'g')
-endfunction
-
-command! StartifySaveBranchSession ':SSave! ' . GetUniqueSessionName()
-command! StartifyLoadBranchSession ':SLoad ' . GetUniqueSessionName()
-]]
--- lua/custom/lib/constant.lua
+++
@@ -1,5 +0,0 @@
-local PREVIOUS_SESSION_LINK_PATH = vim.fn.stdpath('data') .. '/previous_session'
-
-return {
-  PREVIOUS_SESSION_LINK_PATH = PREVIOUS_SESSION_LINK_PATH
-}
