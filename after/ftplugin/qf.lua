66836ec2098f5268b2d8c838f99400070293f995
deptno <deptno@gmail.com>
2023-11-05 21:31:57 +0900

    fix: workaround qf autocmd
    qf 가 한번 열리고 나면 이후 focus 받는 모든 window 에 대해서 이벤트가 트리거됨

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -1,8 +1,19 @@
 vim.api.nvim_create_autocmd({ "BufWinEnter", "BufEnter" }, {
-  group = vim.api.nvim_create_augroup("QfFiletype", {}),
+  group = vim.api.nvim_create_augroup("QuickFixFiletype", { clear = true }),
   callback = function(ev)
-    local opts = {}
+    vim.notify('*warning* autocmd QuickFixFiletype', vim.log.levels.TRACE)
+    if vim.bo.filetype ~= 'qf' then
+      -- FIXME: qf 파일타입에서 뿐 아니라 qf 가 한번 열리면 그 이후로  모든 버퍼에 대해서 호출됨
+      return
+    end
 
-    vim.api.nvim_buf_set_keymap(ev.buf, "n", "q", ":quit<CR>", opts)
+    local opts = {
+      buffer = true
+    }
+    local handler = function ()
+      vim.cmd('quit')
+    end
+
+    vim.keymap.set('n', 'q', handler, opts)
   end
 })
