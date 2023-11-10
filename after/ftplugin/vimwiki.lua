f94d14cabdf2d3c61ec01a27a1fa6b28c8f74d6a
deptno <deptno@gmail.com>
2023-11-10 22:08:01 +0900

    add peek.nvim to preview markdown, vimwiki

--- after/ftplugin/vimwiki.lua
+++ after/ftplugin/vimwiki.lua
@@ -2,14 +2,24 @@ vim.api.nvim_create_autocmd({ "BufWinEnter", "BufEnter" }, {
   pattern = "*.md",
   group = vim.api.nvim_create_augroup("VimwikiFiletype", {}),
   callback = function(ev)
-    local opts = {}
+    local opts = {
+      buffer = true
+    }
+    local handler = function()
+      local peek = require('peek')
 
-    vim.api.nvim_buf_set_keymap(ev.buf, "n", "L", "<Plug>VimwikiDiaryNextDay", opts)
-    vim.api.nvim_buf_set_keymap(ev.buf, "n", "H", "<Plug>VimwikiDiaryPrevDay", opts)
-    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<S-x>", "<Plug>VimwikiToggleListItemj", opts)
-    vim.api.nvim_buf_set_keymap(ev.buf, "n", "tt", ":VimwikiTable<CR>", opts)
-    -- use gx grep instead
-    -- vim.api.nvim_buf_set_keymap(ev.buf, "n", ";w", ":execute 'VWS /' . expand('<cword>') . '/' <Bar> :lopen<CR>", opts) 
-    vim.api.nvim_buf_set_keymap(ev.buf, "n", ";b", ":execute 'VWB' <Bar> :lopen<CR>", opts)
+      if peek.is_open() then
+        peek.close()
+      else
+        peek.open()
+      end
+    end
+
+    vim.keymap.set("n", "L", "<Plug>VimwikiDiaryNextDay", opts)
+    vim.keymap.set("n", "H", "<Plug>VimwikiDiaryPrevDay", opts)
+    vim.keymap.set("n", "<S-x>", "<Plug>VimwikiToggleListItemj", opts)
+    vim.keymap.set("n", "tt", ":VimwikiTable<CR>", opts)
+    vim.keymap.set("n", ";b", ":execute 'VWB' <Bar> :lopen<CR>", opts)
+    vim.keymap.set("n", ";v", handler, opts)
   end
 })
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -393,4 +393,23 @@ return {
     end,
     event = "VeryLazy",
   },
+  {
+    "toppair/peek.nvim",
+    build = "deno task --quiet build:fast",
+    config = function()
+      require("peek").setup({
+        filetype = {
+          'markdown',
+          'vimwiki',
+        },
+        theme = 'light',
+      })
+
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+    event = {
+      "VeryLazy"
+    },
+  },
 }
