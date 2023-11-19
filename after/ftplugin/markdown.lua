d23ae45dd7f074b755e490a09ee1dc862ff9375a
deptno <deptno@gmail.com>
2023-11-19 22:08:42 +0900

    keymap: markdown ;v
    toggle peek

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,19 @@
+vim.api.nvim_create_autocmd({ "BufWinEnter", "BufEnter" }, {
+  group = vim.api.nvim_create_augroup("MarkdownFiletype", {}),
+  callback = function(ev)
+    local opts = {
+      buffer = true
+    }
+    local handler = function()
+      local peek = require('peek')
+
+      if peek.is_open() then
+        peek.close()
+      else
+        peek.open()
+      end
+    end
+
+    vim.keymap.set("n", ";v", handler, opts)
+  end
+})
