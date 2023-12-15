24693ea9111233dcc6ee79aa3e2023820963f9d3
deptno <deptno@gmail.com>
2023-12-16 00:34:19 +0900

    ftp 적용

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -1,8 +1,6 @@
+local ftq = require "custom.lib.ftq"
+
 vim.api.nvim_create_autocmd("BufWinEnter", {
   group = vim.api.nvim_create_augroup("HelpFiletype", {}),
-  callback = function(ev)
-    local opts = {}
-
-    vim.api.nvim_buf_set_keymap(ev.buf, "n", "q", ":bdelete<CR>", opts)
-  end
+  callback = ftq,
 })
--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -1,19 +1,5 @@
+local ftq = require "custom.lib.ftq"
 vim.api.nvim_create_autocmd("BufWinEnter", {
   group = vim.api.nvim_create_augroup("QuickFixFiletype", { clear = true }),
-  callback = function(ev)
-    vim.notify('*warning* autocmd QuickFixFiletype', vim.log.levels.TRACE)
-    if vim.bo.filetype ~= 'qf' then
-      -- FIXME: qf 파일타입에서 뿐 아니라 qf 가 한번 열리면 그 이후로  모든 버퍼에 대해서 호출됨
-      return
-    end
-
-    local opts = {
-      buffer = true
-    }
-    local handler = function ()
-      vim.cmd('quit')
-    end
-
-    vim.keymap.set('n', 'q', handler, opts)
-  end
+  callback = ftq,
 })
