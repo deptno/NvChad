7d4af6028122689998c02013a3a6437b01bedf94
deptno <deptno@gmail.com>
2023-12-17 23:35:11 +0900

    fix: 자바스크립트 주석 타입이 들어감

--- ftdetect/ruby.lua
+++ ftdetect/ruby.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   group = vim.api.nvim_create_augroup("SetKnownRubyFileType", { clear = true }),
   pattern = { "Podfile" },
   callback = function()
-    // @see https://github.com/neovim/neovim/blob/670c7609c85547ef041af8cf17139a396d6af050/runtime/filetype.lua#L31
+    -- @see https://github.com/neovim/neovim/blob/670c7609c85547ef041af8cf17139a396d6af050/runtime/filetype.lua#L31
     vim.api.nvim_cmd({ cmd = "setf", args = { "ruby" } }, {})
   end,
 })
