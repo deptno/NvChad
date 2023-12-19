ba16efac796b68a52b37980704ae6b6dccc84544
deptno <deptno@gmail.com>
2023-12-20 00:19:41 +0900

    infixes와 suffixes 를 받아서 형제 출력하는 로직 구현

--- lua/custom/lib/get_suffix_siblings.lua
+++ lua/custom/lib/get_suffix_siblings.lua
@@ -1,83 +1,45 @@
-local file_exists = require('custom.lib.file_exists')
-local map         = require('custom.lib.map')
+local file_exists = require("custom.lib.file_exists")
+local map = require("custom.lib.map")
+local slice = require("custom.lib.slice")
+local every = require("custom.lib.every")
+local is_equal = require("custom.lib.is_equal")
+local some     = require("custom.lib.some")
 
 ---주어진 infixes 를 기반으로 infix 가 없는 경우를 포함하여 존재하는 파일 목록을 리턴
 ---TODO: infix 가 여러개인 경우도 존재함 i.g. android.www
 ---@param infixes table(string) filepath
 ---@param path string filepath
 ---@return table|nil
-local get_suffix_siblings = function (infixes, path)
+local get_suffix_siblings = function(infixes, suffixes, path)
   local dirname = vim.fs.dirname(path)
   local filename = vim.fs.basename(path)
   local parts = vim.split(filename, '%.')
 
   if #parts <= 1 then
-    return vim.notify(string.format('invalid filename %s', filename), vim.log.levels.DEBUG)
+    return vim.notify(string.format("invalid filename %s", filename), vim.log.levels.DEBUG)
   end
 
   local prefix = parts[1]
   local suffix = parts[#parts]
 
-  if not (suffix == 'ts' or suffix == 'tsx') then
-    return vim.notify(string.format('unsupported file ext %s', suffix), vim.log.levels.DEBUG)
+  if not some(is_equal(suffix), suffixes) then
+    return vim.notify(string.format("unsupported file ext %s", suffix), vim.log.levels.DEBUG)
   end
 
   local siblings = {}
+  local files = map(vim.fs.basename, vim.fn.glob(dirname .. "/*." .. suffix, false, true))
 
-  -- index.ts
-  if #parts == 2 then
-    for _, infix in ipairs(infixes) do
-      local sibling = table.concat({ prefix, infix, suffix }, '.')
-
-      if file_exists(string.format('%s/%s', dirname, sibling)) then
-        table.insert(siblings, sibling)
-      end
-    end
-  else
-    -- index.index.ts
-    local infix = parts[#parts - 1]
-    local has_infix = false
-    for _, sibling_infix in ipairs(infixes) do
-      if infix == sibling_infix then
-        has_infix = true
-      end
-    end
-
-    local prefix_table = {}
-    local prefix_end_index = has_infix and #parts - 2 or #parts - 3
-
-    for i = 1, prefix_end_index do
-      table.insert(prefix_table, parts[i])
-    end
-
-    prefix = table.concat(prefix_table, '.')
-
-    -- index.{android,ios}.ts
-    if has_infix then
-      -- insert index.ts
-      local sibling = table.concat({ prefix, suffix }, '.')
-
-      if file_exists(string.format('%s/%s', dirname, sibling)) then
-        table.insert(siblings, sibling)
-      end
-
-      for _, sibling_infix in ipairs(infixes) do
-        if infix ~= sibling_infix then
-          sibling = table.concat({ prefix, sibling_infix, suffix }, '.')
-
-          if file_exists(string.format('%s/%s', dirname, sibling)) then
-            table.insert(siblings, sibling)
+  for _, f in ipairs(files) do
+    if f ~= filename then
+      local _parts = vim.split(f, "%.")
+      if prefix == _parts[1] then
+        if #_parts == 2 then
+          table.insert(siblings, f)
+        elseif #_parts >= 3 then
+          local _infixes = slice(2, #_parts - 1, _parts)
+          if every(function (_i) return some(is_equal(_i), infixes) end, _infixes) then
+            table.insert(siblings, f)
           end
-        end
-      end
-    else
-      -- index.not-infix.ts
-      for _, sibling_infix in ipairs(infixes) do
-        -- index.not-infix.{www,ios}.ts
-        local sibling = table.concat({ prefix, sibling_infix, suffix }, '.')
-
-        if file_exists(string.format('%s/%s', dirname, sibling)) then
-          table.insert(siblings, sibling)
         end
       end
     end
@@ -87,8 +49,8 @@ local get_suffix_siblings = function (infixes, path)
     return
   end
 
-  local with_dir = function (file)
-    return string.format('%s/%s', dirname, file)
+  local with_dir = function(file)
+    return string.format("%s/%s", dirname, file)
   end
 
   return map(with_dir, siblings)
